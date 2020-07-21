8a09ce19567e73d849e41436a96bcfcd4d2a296d
renataalmeids <ras_almeida@hotmail.com>
2020-07-21 19:23:23 -0300

    Instalações do Firebase e React-DOM

---
+++ src/Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKgyumyFgj8t4jVAuCNoLNEJzIddIAFlo",
+    authDomain: "burger-queen-407c9.firebaseapp.com",
+    databaseURL: "https://burger-queen-407c9.firebaseio.com",
+    projectId: "burger-queen-407c9",
+    storageBucket: "burger-queen-407c9.appspot.com",
+    messagingSenderId: "797335097838",
+    appId: "1:797335097838:web:86121060b369aaaaff7eff"
+    };
+
+    //export defaul firebaseConfig;
