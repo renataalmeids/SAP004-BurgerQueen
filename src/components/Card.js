265ff6200319cb4954f88a57d776d79c6fae0976
renataalmeids <ras_almeida@hotmail.com>
2020-08-14 09:16:19 -0300

    função de status pedido finalizada

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
+import firebase from '../config/Config';
 import Button from '../components/Button';
 
 
 
 const Cardkitchen = (props) => {
     const [status, setStatus] = useState(props.pedido.status);
-const handleClick = () => {
-    setStatus('Pronto')
-}
-
-// const updateStatus = (doc) =>{
-
-//     firestore.collection('client').doc(doc.id).update({
-//         status:'Pronto',
-//     })
+    const handleClick = (pedido) => {
+        firebase.firestore().collection('pedidos').doc(pedido.id).update({
+            status: 'Pronto',
+        })
+        setStatus('Pronto')
+    }
 
 
     return (
@@ -24,10 +22,9 @@ const handleClick = () => {
             <p className="pedido">Pedido: {props.pedido.pedido.map((item, index) =>
                 <span key={index}>{item.name} X {item.count}</span>
             )}</p>
-            {/* <Button className={'btn-status'} onClick={handleClick} name='Pedido Pronto' /> */}
-            <button className={'btn-status'} onClick={() =>handleClick()}>Pedido Pronto</button>
+            <button className={'btn-status'} onClick={() => handleClick(props.pedido)}>Pedido Pronto</button>
         </div>
     )
-}; 
+};
 
 export default Cardkitchen;
