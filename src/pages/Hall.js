00fdbb156267d4a6acc6bcce2e50728e86a30990
Nicole <nicole.franca.goncalves@gmail.com>
2020-08-14 11:08:45 -0300

    Deletando form

--- src/pages/Hall.js
+++ src/pages/Hall.js
@@ -147,21 +147,6 @@ const Hall = () => {
                 ))}
                 <p>Total:R${total},00</p>
             </div>
-            <form className='burger-type'>
-                <label htmlFor='radio-hall' >
-                    <Input name='burgerType' type='radio' value='Carne bovina'/>
-                    Carne bovina
-                </label>
-                <label htmlFor='radio-hall' >
-                    <Input name='burgerType' type='radio' value='Frango'/>
-                    Frango
-                </label>
-                <label htmlFor='radio-hall' >
-                    <Input name='burgerType' type='radio' value='Vegetariano'/>
-                    Vegetariano
-                </label>
-                <ButtonHall className='btn-menu' name='Enviar'/>
-            </form>
         </main>
     )
 };
