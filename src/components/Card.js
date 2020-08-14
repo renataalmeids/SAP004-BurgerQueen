3f84c7f269eb5966d3f6ac7e204fb35ccf497796
renataalmeids <ras_almeida@hotmail.com>
2020-08-14 11:12:04 -0300

    deletando componente sem uso

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import firebase from '../config/Config';
-import Button from '../components/Button';
 
 
 
@@ -19,9 +18,9 @@ const Cardkitchen = (props) => {
             <p className="mesa">Mesa: {props.pedido.mesa}</p>
             <p className="cliente">Cliente: {props.pedido.client}</p>
             <p className="status-pedding">Status: {status}</p>
-            <p className="pedido">Pedido: {props.pedido.pedido.map((item, index) =>
-                <span key={index}>{item.name} X {item.count}</span>
-            )}</p>
+            <div className="pedido">Pedido: {props.pedido.pedido.map((item, index) =>
+                <p key={index}>{item.name} X {item.count}</p>
+            )}</div>
             <button className={'btn-status'} onClick={() => handleClick(props.pedido)}>Pedido Pronto</button>
         </div>
     )
